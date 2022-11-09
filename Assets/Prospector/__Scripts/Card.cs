ae1ebde4a1b040c02593536791b985b59b69a098
1ChaseLawrence1 <chaselawrence243@gmail.com>
2022-11-08 19:39:01 -0500

    Fixing Card Script / working on menu

--- Assets/Prospector/__Scripts/Card.cs
+++ Assets/Prospector/__Scripts/Card.cs
@@ -1,55 +1,117 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class Card : MonoBehaviour {
+public class Card : MonoBehaviour
+{
+	public string suit; //Suit of the card (C, D, H, or S)
+	public int rank; //Rank of the Card (1-14)
+	public Color color = Color.black; //Color to tint pips
+	public string colS = "Black"; //or "Red". Name of the color.
 
-	public string    suit;
-	public int       rank;
-	public Color     color = Color.black;
-	public string    colS = "Black";  // or "Red"
-	
+	//This List holds all of the Decorator GameObjects
 	public List<GameObject> decoGOs = new List<GameObject>();
+
+	//This List holds all of the Pip GameObjects
 	public List<GameObject> pipGOs = new List<GameObject>();
-	
-	public GameObject back;  // back of card;
-	public CardDefinition def;  // from DeckXML.xml		
 
+	public GameObject back; //The GameObject of the back of the card
+	public CardDefinition def; //Parsed from DeckXML.xml
 
-	public bool faceUp {
-		get {
+	//List of the SpriteRenderer Components of this GameObject and its children
+	public SpriteRenderer[] spriteRenderers;
+
+	void Start()
+	{
+		SetSortOrder(0); //Ensures that the card starts properly depth sorted
+	}
+
+	public bool faceUp
+	{
+		get
+		{
 			return (!back.activeSelf);
 		}
-
-		set {
+		set
+		{
 			back.SetActive(!value);
 		}
 	}
 
-
-	// Use this for initialization
-	void Start () {
-	
+	//If spriteRenderers is not yet defined, this function defines it
+	public void PopulateSpriteRenderers()
+	{
+		//If spriteRenderers is null or empty
+		if (spriteRenderers == null || spriteRenderers.Length == 0)
+		{
+			//Get SpriteRenderer Components of this GameObject and its children
+			spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-} // class Card
 
-[System.Serializable]
-public class Decorator{
-	public string	type;			// For card pips, tyhpe = "pip"
-	public Vector3	loc;			// location of sprite on the card
-	public bool		flip = false;	//whether to flip vertically
-	public float 	scale = 1.0f;
+	//Sets the SortingLayerName on all SpriteRenderer Components
+	public void SetSortingLayerName(string tSLN)
+	{
+		PopulateSpriteRenderers();
+
+		foreach (SpriteRenderer tSR in spriteRenderers)
+		{
+			tSR.sortingLayerName = tSLN;
+		}
+	}
+
+	//Sets the sortingOrder of all SpriteRenderer Components
+	public void SetSortOrder(int sOrd)
+	{
+		PopulateSpriteRenderers();
+
+
+		//Iterate through all the spriteRenderers as tSR
+		foreach (SpriteRenderer tSR in spriteRenderers)
+		{
+			if (tSR.gameObject == this.gameObject)
+			{
+				//if the GameObject is this.gameObject, it's the background
+				tSR.sortingOrder = sOrd; //Set its order to sOrd
+				continue; //And continue to the next iteration of the loop
+			}
+			//Each of the children of this GameObject are named. Switch based on the names
+			switch (tSR.gameObject.name)
+			{
+			case "back": //If the name is "back"
+				tSR.sortingOrder = sOrd + 2; //Set it to the highest layer to cover everything else
+				break;
+			case "face": //If the name is "face"...
+			default: //or if it's anything else
+				tSR.sortingOrder = sOrd + 1; //Set it to the middle layer to be above the background
+				break;
+			}
+		}
+	}
+
+	virtual public void OnMouseUpAsButton()
+	{
+		//print(name);
+	}
 }
 
 [System.Serializable]
-public class CardDefinition{
-	public string	face;	//sprite to use for face cart
-	public int		rank;	// value from 1-13 (Ace-King)
-	public List<Decorator>	
-					pips = new List<Decorator>();  // Pips Used
+public class Decorator
+{
+	//This class stores information about each decorator or pop from DeckXML
+	public string type; //For card pips, type = "pip"
+	public Vector3 loc; //The location of the Sprite on the card
+	public bool flip = false; //Whether to flip the Sprite vertically
+	public float scale = 1f; //The scale of the sprite
+}
+
+[System.Serializable]
+public class CardDefinition
+{
+	//This class stores information for each rank of card.
+	public string face; //Sprite to use for each face card
+	public int rank; //The rank (1-13) of this card
+
+	
+	public List<Decorator> pips = new List<Decorator>(); 
 }
